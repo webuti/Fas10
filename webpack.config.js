764eda42490e85b8fbbe5df71cee7e7dd22beb59
omer <qmerfp@me.com>
2021-03-31 22:19:09 +0300

    dil düzenlemeleri

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,10 @@ module.exports = {
             '@': path.resolve('resources/js'),
         },
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false
+        })
+    ]
 };
