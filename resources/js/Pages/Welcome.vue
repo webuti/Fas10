5ad8017b6a62546e2f7219aba9a2f9669e25b11a
omer <qmerfp@me.com>
2021-03-31 22:16:00 +0300

    dil düzenlemeleri

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -6,34 +6,22 @@
 
             <div class="sm:text-center items-center lg:text-center  ">
 
-                <div class="wordCarousel  ">
-                    <ul class="flip2">
-                        <li>
-                            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-4xl  lg:text-5xl">
-                                <span class="block xl:inline   ">Fason'a fayda odaklı </span> <span
-                                class="block   xl:inline bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-400 inline-block">teknoloji</span>
-                            </h1>
-                        </li>
-                        <li>
-                            <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-4xl lg:text-5xl">
-
-                                <span class="block xl:inline  ">Fas10  fayda odaklı </span> <span
-                                class="block   xl:inline bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-400 inline-block">teknoloji</span>
-                            </h1></li>
-                    </ul>
-                </div>
+                <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-3xl md:text-4xl  lg:text-5xl">
+                    <span class="block xl:inline   ">Fason'a fayda odaklı </span> <span
+                    class="block   xl:inline bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-green-400 inline-block">teknoloji</span>
+                </h1>
 
                 <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg  md:mt-5 md:text-xl ">
                     Fas10 işler için, fason çalışan şirket kataloğu, iş takip yönetimi
                 </p>
-                <div class="mt-5 sm:mt-8 flex  items-center justify-center">
+                <div class="mt-5 sm:mt-8 flex space-x-2 items-center justify-center">
                     <div class="rounded-md shadow">
                         <inertia-link href="/register"
                                       class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-green-600 hover:bg-green-700 md:py-4 md:text-lg md:px-10">
                             Hemen kayıt ol
                         </inertia-link>
                     </div>
-                    <div class="  sm:ml-3">
+                    <div class=" rounded-md shadow sm:ml-3">
                         <a href="#"
                            class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-green-700 bg-green-100 hover:bg-green-200 md:py-4 md:text-lg md:px-10">
                             Videoyu incele
