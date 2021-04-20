a86f10382edc681ee315120b562b886575f31191
omer <qmerfp@me.com>
2021-04-21 01:34:27 +0300

    fix

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -37,8 +37,7 @@
             </div>
             <ul class="flex items-center hidden space-x-8 lg:flex">
                 <button
-                    id="switchTheme"
-                    @click="lightTheme = !lightTheme"
+                        @click="switchTheme()"
                     class="h-10 w-10 flex justify-center items-center focus:outline-none"
                 >
                     <svg
@@ -120,30 +119,63 @@
                 </template>
             </ul>
             <div class="lg:hidden">
-                <button
-                    aria-label="Open Menu"
-                    title="Open Menu"
-                    class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
-                    @click="isMenuOpen = true"
-                >
-                    <svg
-                        class="w-5 text-gray-600 dark:text-gray-300"
-                        viewBox="0 0 24 24"
+                <div class="flex">
+                    <button
+                        @click="switchTheme()"
+                        class="h-10 w-10 flex justify-center items-center focus:outline-none"
                     >
-                        <path
+                        <svg
+                            v-if="lightTheme"
+                            class="w-6 text-yellow-500 h-6"
                             fill="currentColor"
-                            d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-                        ></path>
-                        <path
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill-rule="evenodd"
+                                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                                clip-rule="evenodd"
+                            ></path>
+                        </svg>
+
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            v-else
+                            class="w-6 text-gray-500 h-6"
+                            viewBox="0 0 20 20"
                             fill="currentColor"
-                            d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-                        ></path>
-                        <path
-                            fill="currentColor"
-                            d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-                        ></path>
-                    </svg>
-                </button>
+                        >
+                            <path
+                                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+                            />
+                        </svg>
+                    </button>
+
+                    <button
+                        aria-label="Open Menu"
+                        title="Open Menu"
+                        class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+                        @click="isMenuOpen = true"
+                    >
+                        <svg
+                            class="w-5 text-gray-600 dark:text-gray-300"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                            ></path>
+                            <path
+                                fill="currentColor"
+                                d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                            ></path>
+                            <path
+                                fill="currentColor"
+                                d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
                 <div
                     v-if="isMenuOpen"
                     class="absolute top-0 z-20 left-0 w-full"
@@ -313,19 +345,22 @@ export default {
             document.querySelector("html").classList.add("dark");
         }
 
-        document
-            .getElementById("switchTheme")
-            .addEventListener("click", function () {
-                let htmlClasses = document.querySelector("html").classList;
+    },
+    methods: {
+        switchTheme() {
+
+             let htmlClasses = document.querySelector("html").classList;
                 if (localStorage.theme == "dark") {
                     htmlClasses.remove("dark");
+                       this.lightTheme = false;
                     localStorage.removeItem("theme");
                 } else {
                     htmlClasses.add("dark");
                     this.lightTheme = true;
                     localStorage.theme = "dark";
                 }
-            });
+
+        },
     },
     props: {
         canLogin: Boolean,
